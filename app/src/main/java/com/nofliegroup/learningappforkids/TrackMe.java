94c06d1ebd3c4cd2a650deeff207dcbca615e621
Momodou <mnjie@nofliegroup.com>
2023-01-29 23:12:15 +0100

    final commit

---
+++ app/src/main/java/com/nofliegroup/learningappforkids/TrackMe.java
@@ -0,0 +1,14 @@
+package com.nofliegroup.learningappforkids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrackMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track_me);
+    }
+}
